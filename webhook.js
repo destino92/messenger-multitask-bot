f1adc79871a4ae1ff637b9afede6e3d9614ec188
destino92 <destinodello@gmail.com>
2017-03-07 16:42:39 +0300

    feat: added get webhook request for facebook validation

--- webhook.js
+++ webhook.js
@@ -7,3 +7,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
+
+/* For Facebook Validation */
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'haji_no_moto') {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.status(403).end();
+  }
+});
