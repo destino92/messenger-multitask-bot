95eb6c105e23cd4e627c3cbdda0fb5d2345eb1c3
destino92 <destinodello@gmail.com>
2017-03-08 00:01:46 +0300

    fix: changed how server is defined and initialized

--- webhook.js
+++ webhook.js
@@ -2,16 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const server = app.listen(process.env.PORT || 5000, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-});
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
-});
+})
 
 /* For Facebook Validation */
 app.get('/webhook', (req, res) => {
@@ -22,6 +26,11 @@ app.get('/webhook', (req, res) => {
     res.status(403).end();
   }
 });
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'));
+})
 
 /* Handling all messenges */
 app.post('/webhook', (req, res) => {
