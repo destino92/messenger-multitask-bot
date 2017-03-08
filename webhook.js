d313c5c09a19e0261daf2d816aaea35df1367826
destino92 <destinodello@gmail.com>
2017-03-08 22:50:41 +0300

    fix: require request in webhook.js

--- webhook.js
+++ webhook.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
