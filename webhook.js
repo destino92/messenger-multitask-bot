9519ea66bead3090eda52515f97eb651cc765465
destino92 <destinodello@gmail.com>
2017-03-08 22:26:30 +0300

    feat: added send message method to respond to user message with an echo message

--- webhook.js
+++ webhook.js
@@ -31,3 +31,25 @@ app.post('/webhook', (req, res) => {
     res.status(200).end();
   }
 });
+
+// Send Messages to user
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: PAGE_ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, function(error, response) {
+    if (error) {
+
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
