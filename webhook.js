8afba264b6b670243a8b04605a0483126d8147ce
destino92 <destinodello@gmail.com>
2017-03-09 13:25:29 +0300

    updated Get webhook method

--- webhook.js
+++ webhook.js
@@ -14,16 +14,18 @@ const server = app.listen(process.env.PORT || 5000, () => {
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send('Hello world, I am a chat bot')
 })
 
 /* For Facebook Validation */
 app.get('/webhook/', (req, res) => {
   if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'haji_no_moto') {
+    console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
-    res.status(403).end();
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.status(403);
   }
 });
 
